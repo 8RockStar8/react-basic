2b68a4dd023ca1d83dc0849cd2503e6aae0a3d28
RockStar <gagik.aproyan.dev@gmail.com>
2020-02-06 20:29:16 +0400

    map example

--- src/app/content/comment/component.jsx
+++ src/app/content/comment/component.jsx
@@ -8,20 +8,42 @@ function formatDate(date) {
     return date.toLocaleDateString();
 }
 
-function Comment(props) {
-    console.log('es gtnvum em comment-i mej', props);
-    
-    return (
-        <div className="Comment">
-            <UserInfo author={props.user.author} />
-            <div className="Comment-text">
-                {props.user.text}
+class Comment extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numbers: [1, 2, 3, 5, 6, 7, 4, 9]
+        }
+    }
+
+    inch = (num) => {
+        let met = num.map((item, index) => {
+            return (
+                <li key={index} className='esimInch'>
+                    <span>{item}</span>
+                </li>
+            )
+        })
+        console.log(met);
+        return met
+    }
+
+    render() {
+        return (
+            <div className="Comment">
+                <UserInfo author={this.props.user.author} />
+                <div className="Comment-text">
+                    {this.props.user.text}
+                </div>
+                <div className="Comment-date">
+                    {formatDate(this.props.user.date)}
+                </div>
+                <ul>
+                    {this.inch(this.state.numbers)}
+                </ul>
             </div>
-            <div className="Comment-date">
-                {formatDate(props.user.date)}
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Comment;
